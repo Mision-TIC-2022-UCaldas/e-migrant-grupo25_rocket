39d4936abcad24b9b9cfc3d2c0fc7d34f03e8cf9
Alucard Belmont <66456446+DarkCastlevania@users.noreply.github.com>
2021-11-27 01:26:56 -0500

    listado de personas

--- Frontend/Pages/Migrante/DetailsMigrante.cshtml.cs
+++ Frontend/Pages/Migrante/DetailsMigrante.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia;
 
 namespace MyApp.Namespace
 {
     public class DetailsMigranteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMigrante _repo;
+        public Migrante migrante { get; set;}
+    
+        public DetailsMigranteModel(IRepositorioMigrante repo)
         {
+            _repo = repo;
+        }
+         
+
+        public void OnGet(int id)
+        {
+            migrante = _repo.GetMigrante(id);
         }
     }
 }
--- Frontend/Pages/Migrante/EditMigrante.cshtml.cs
+++ Frontend/Pages/Migrante/EditMigrante.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MyApp.Namespace
 {
     public class EditMigranteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMigrante _repo;
+        public Migrante migrante { get; set;}
+        public EditMigranteModel(IRepositorioMigrante repo)
         {
+            _repo = repo;
+        }
+         public void OnPost(Migrante migrante)
+        {             
+            _repo.UpdateMigrante(migrante);
+        }
+        public void OnGet(int id)
+        {
+            migrante = _repo.GetMigrante(id);
         }
     }
 }
